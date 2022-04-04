65952d526b3f812bb8f62be7795d42cd6820a18e
tyakamyz <tyakamyz@naver.com>
2022-04-04 23:05:07 +0900

    feat: JPQL 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,12 +16,12 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member = em.find(Member.class, 2L);
-            System.out.println("member.getId(): " + member.getId());
-            System.out.println("member.getName(): " + member.getName());
-            member.setName("HelloJpa");
+            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
 
-            tx.commit();
+            for (Member member : members) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
         }catch (Exception e){
             tx.rollback();
         }finally {
