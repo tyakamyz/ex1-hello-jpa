248458a3f6fe93cc8acd0b00962de7f485a4c5ec
tyakamyz <tyakamyz@naver.com>
2022-04-18 22:34:04 +0900

    feat: GenerationType.SEQUENCE 테스트

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,10 +3,11 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
 
     @Column(name = "name", nullable = false, length = 100)
